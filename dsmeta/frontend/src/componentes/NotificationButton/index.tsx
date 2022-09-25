66753aaf2f0ec748719991e35380ee55281547ca
ThiagoAnd <thiago.andrade.it@outlook.com>
2022-09-25 10:35:17 -0300

    Feat: Atualização de projeto

---
+++ dsmeta/frontend/src/componentes/NotificationButton/index.tsx
@@ -0,0 +1,11 @@
+//notficaion
+
+function NotificationButton() {
+    return (
+            <div className="dsmeta-red-btn">
+                <img src="notification-icon.svg" alt="Notificar" />
+            </div>
+    )
+}
+
+export default NotificationButton
