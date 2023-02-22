70b0f63364176213f2b0ac302109f7ada012699a
ThiagoAnd <thiago.andrade.it@outlook.com>
2023-02-22 20:27:40 -0300

    Feat: Project updated

--- dsmeta/frontend/src/App.tsx
+++ dsmeta/frontend/src/App.tsx
@@ -3,7 +3,7 @@ import NotificationButton from "./componentes/NotificationButton"
 function App() {
   return (
     <>
-    <h1>Pagina inicial</h1>
+    <h1>Pagina inicial. https://github.com/acenelio/dsmeta-css</h1>
     <NotificationButton/>
     </>
   )
